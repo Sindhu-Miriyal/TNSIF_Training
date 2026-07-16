ffa2073d1ccad31d1fb408ab711cfa146702b8e2
sindh <sindh@HP-P15>
2026-07-16 11:01:14 +0530

    First commit

---
+++ TNSIF_Training/src/org/tnsif/acc/c2tc/java/VariableDemo.java
@@ -0,0 +1,18 @@
+package org.tnsif.acc.c2tc.java;
+
+public class VariableDemo {
+	int price=10;
+	void print() {
+		String msg="hello";
+		System.out.println(msg);
+	}
+	static String mesage="Hello Students";
+	public static void main(String[] args) {
+		VariableDemo obj=new VariableDemo();
+		System.out.println(obj.price);
+		obj.print();
+		System.out.println(mesage);
+		
+	}
+	
+}
