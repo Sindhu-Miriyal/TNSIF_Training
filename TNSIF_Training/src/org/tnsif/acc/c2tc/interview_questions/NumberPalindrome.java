61a018ec977055a8208aed451ac990db8742516d
sindh <sindh@HP-P15>
2026-07-20 10:36:10 +0530

    First commit

--- TNSIF_Training/src/org/tnsif/acc/c2tc/interview_questions/NumberPalindrome.java
+++ TNSIF_Training/src/org/tnsif/acc/c2tc/interview_questions/NumberPalindrome.java
@@ -1,5 +1,28 @@
 package org.tnsif.acc.c2tc.interview_questions;
 
-public class NumberPalindrome {
 
+
+import java.util.Scanner;
+
+public class NumberPalindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int original = num;
+        int reverse = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+
+        if (original == reverse)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
 }
--- TNSIF_Training/src/org/tnsif/acc/c2tc/interview_questions/ReverseString.java
+++ TNSIF_Training/src/org/tnsif/acc/c2tc/interview_questions/ReverseString.java
@@ -1,28 +1,5 @@
 package org.tnsif.acc.c2tc.interview_questions;
 
-//import java.util.Scanner;
-//
-//public class ReverseString {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//
-//        System.out.print("Enter a string: ");
-//        String str = sc.nextLine();
-//
-//        StringBuilder rev = new StringBuilder();
-//
-//        for (int i = str.length() - 1; i >= 0; i--) {
-//            rev.append(str.charAt(i));
-//        }
-//
-//        System.out.println("Reversed String: " + rev.toString());
-//
-//        sc.close();
-//    }
-//}
-
-
-
 import java.util.Scanner;
 
 public class ReverseString {
@@ -30,10 +7,7 @@ public class ReverseString {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String str = sc.nextLine();
-
-        // Easy way: use StringBuilder's reverse()
         String rev = new StringBuilder(str).reverse().toString();
-
         System.out.println("Reversed String: " + rev);
         sc.close();
     }
