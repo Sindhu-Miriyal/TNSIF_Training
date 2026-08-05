d3f5b35d4df4eaef43c9aebca00a1c3645e2ccf4
sindh <sindh@192.168.1.5>
2026-08-05 10:27:00 +0530

    First commit

---
+++ TNSIF_Training/src/org/tnsif/acc/c2tc/scannerclass_bufferreader/ScannerDemo2.java
@@ -0,0 +1,19 @@
+package org.tnsif.acc.c2tc.scannerclass_bufferreader;
+import java.util.Scanner;
+public class ScannerDemo2 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter your name:");
+		String name=scan.nextLine();
+		System.out.println("Enter your age:");
+		int age=scan.nextInt();
+		System.out.println("Enter your height:");
+		float height=scan.nextFloat();
+		System.out.println("Are you a student");
+		
+		
+
+	}
+
+}
